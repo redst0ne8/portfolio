bcf44d59715bb2a496f85d5c6914fa0542d4be7e
redst0ne8 <redst0ne8.discord@gmail.com>
2026-04-27 07:25:36 -0400

    wawa

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const slideshowImages = [
     'images/slideshow/slide1.jpg',
     'images/slideshow/slide2.jpg',
     'images/slideshow/slide3.jpg',
-    'images/slideshow/slide4.jpg'
+    'images/slideshow/slide4.jpg',
+    'images/slideshow/slide5.jpg',
+    'images/slideshow/slide6.jpg',
+    'images/slideshow/slide7.jpg',
+    'images/slideshow/slide8.jpg',
+    'images/slideshow/slide9.jpg',
+    'images/slideshow/slide10.jpg',
 ];
 
 const slide1 = document.getElementById('slide1');
